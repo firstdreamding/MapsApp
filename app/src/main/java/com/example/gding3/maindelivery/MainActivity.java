cef37b168081e4568ccbaf63620790d1e8d2d2ac
Weijiang Li <wl13@illinois.edu>
2018-12-11 17:20:33 -0600

    fixed the drawer and its in the main page now

--- app/src/main/java/com/example/gding3/maindelivery/MainActivity.java
+++ app/src/main/java/com/example/gding3/maindelivery/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gding3.maindelivery;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
