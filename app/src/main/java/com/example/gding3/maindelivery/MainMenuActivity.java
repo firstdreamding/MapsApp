50437daac859d71578887113ccbb12b41740f0bb
firstdreamding <huaxu3721@yahoo.com>
2018-12-11 18:16:01 -0600

    Revert "Merge remote-tracking branch 'github/master'"
    This reverts commit 915feaaa47eedcfcf178818307c8f80ff1013e38, reversing
    changes made to 16a07fc060d96c9ddcb44f601e2774849011de82.

--- app/src/main/java/com/example/gding3/maindelivery/MainMenuActivity.java
+++ app/src/main/java/com/example/gding3/maindelivery/MainMenuActivity.java
@@ -1,36 +1,18 @@
 package com.example.gding3.maindelivery;
 
 import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toolbar;
 
-
-public class MainMenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
+public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
         configureNextButton();
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer,  R.string.openDrawer, R.string.closeDrawer);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
@@ -51,28 +33,4 @@ public class MainMenuActivity extends AppCompatActivity implements NavigationVie
         });
 
     }
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_account) {
-            // Handle the account action
-        } else if (id == R.id.nav_favourites) {
-
-        } else if (id == R.id.nav_home) {
-
-        } else if (id == R.id.nav_notifications) {
-
-        } else if (id == R.id.nav_settings) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-
 }
